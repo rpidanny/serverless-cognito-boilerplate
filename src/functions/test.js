84cf4c6d3bd8e4574118a4d76a4cb190e8e3ed24
rpidanny <abhishekmaharjan1993@gmail.com>
2019-08-14 15:13:21 +0200

    Update test function name

--- src/functions/test.js
+++
@@ -1,6 +0,0 @@
-const { success } = require('../utils/responses')
-
-module.exports.handler = (event, context, callback) => {
-  console.log(JSON.stringify(event, null, 2))
-  callback(null, success({ data: 'OK' }))
-}
