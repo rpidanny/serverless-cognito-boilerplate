f7d85140586ac50c9422c969299890327427ec2f
rpidanny <abhishekmaharjan1993@gmail.com>
2019-08-15 11:29:12 +0200

    Update function response

--- src/functions/helloworld.js
+++ src/functions/helloworld.js
@@ -2,5 +2,8 @@ const { success } = require('../utils/responses')
 
 module.exports.handler = (event, context, callback) => {
   // console.log(JSON.stringify(event, null, 2))
-  callback(null, success({ data: 'OK' }))
+  callback(null, success({
+    message: 'Go Serverless v1.0! Your function executed successfully!',
+    input: event
+  }))
 }
